7f009e54caee4e84d7fbe2377c09d08fb8b5cfc0
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-06-12 10:20:29 +0530

    generics in classes

--- production/src/generic.ts
+++ production/src/generic.ts
@@ -49,7 +49,7 @@ function loggingIdentity2<Type>(arg: Array<Type>): Array<Type> { //return type i
     return products[myIndex]
 }
 
-//above function using arro functions
+//above function using arrow functions
 const getMoreSearchProducts = <T,>(products: T[]): T => { //<T> mans it is going to be generic  //<T,> is used so that it can be differentiate with h1 or p tag while using jsx or tsx
     //do some database operations
     const myIndex = 4
@@ -57,9 +57,50 @@ const getMoreSearchProducts = <T,>(products: T[]): T => { //<T> mans it is going
 }
 
 
+//generic type parameter
+interface database{
+    connection:string,
+    username:string,
+    password:string
+}
 
 
 
+function anotherFunctio<T extends database,U extends number>(valOne:T,valTwo:U):object{  //extends number means u must have number or properties of a number
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+
+// anotherFunctio(3,"4") // this will cause an error as valTwo is string which extends to number
+// anotherFunctio(3,4.5)  //now val 1 should be database as it extends to it 
+anotherFunctio({connection:"p",username:"p",password:"p"},4.5)
+
+//generic classes
+
+//lets we have two sellable items
+
+interface quiz{
+    name:string,
+    type:string
+    price:number
+}
+interface course{
+    name:string,
+    author:string,
+    subject:string,
+    price:number
+}
+
+class Sellable <T>{  //try to use it in iiitu canteen
+    public cart:T[]=[]  //we have a class sellable which is of type t have array of type t
+
+    addToCart(product:T){  //this method push product in cart
+        this.cart.push(product);
+    }
+}
 
 
 
